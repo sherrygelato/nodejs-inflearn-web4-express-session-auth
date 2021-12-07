0eb3826a2455188c6077c5f6e177fad5a630beb9
sherrygelato <sherrygelato@kakao.com>
2021-12-07 13:45:27 +0900

    2. express-session 옵션

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,35 +1,25 @@
 var express = require('express')
 var parseurl = require('parseurl')
-var session = require('express-session')
+var session = require('express-session') // 모듈로서 사용하기
 
 var app = express()
 
+// 사용자의 요청이 있을 때마다 사용하기
 app.use(session({
-  secret: 'keyboard cat',
+    // session함수 실행시키면 세션이 실행되고
+    // 미들웨어가 내부적으로 개입해서 status 작업 실행함
+    // 이하 옵션 핸들링
+  secret: 'keyboard cat', // 필수요건 => 개인 정보니, 별도 파일에 
   resave: false,
+  // default = false 세션 데이터 바뀌기 전까진 저장소에 저장 안함
   saveUninitialized: true
+  // default = true 세션 필요전까지 세션 구동 안한다. 
+  // false는 무조건 구동이기에, 서버에 큰 부담
 }))
 
-app.use(function (req, res, next) {
-  if (!req.session.views) {
-    req.session.views = {}
-  }
-
-  // get the url pathname
-  var pathname = parseurl(req).pathname
-
-  // count the views
-  req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
-
-  next()
-})
-
-app.get('/foo', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
-})
-
-app.get('/bar', function (req, res, next) {
-  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+app.get('/', function (req, res, next) {
+//   res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+    res.send('Hello session!')
 })
 
 app.listen(3000, function () {
